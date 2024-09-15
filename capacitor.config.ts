d5b8e25b1f3e4ed7bbc6307706e5d7d0d35030dd
Victor-matos99 <victor.matosca@anahuac.mx>
2024-09-15 02:15:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'XVYEARS',
+  webDir: 'www'
+};
+
+export default config;
